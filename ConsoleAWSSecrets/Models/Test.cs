81ea1ee7f03dd99d6a076c24cca5015fa3386a5e
Werku López López <werku.lopez@tajamar365.com>
2024-05-23 10:19:21 +0200

    commit

---
+++ ConsoleAWSSecrets/Models/Test.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAWSSecrets.Models
+{
+    public class Test
+    {
+        private KeysModel keys;
+
+        public Test(KeysModel keys)
+        {
+            this.keys = keys;
+        }
+
+        public string GetValue()
+        {
+            return this.keys.Api;
+        }
+    }
+}
--- ConsoleAWSSecrets/Program.cs
+++ ConsoleAWSSecrets/Program.cs
@@ -1,14 +1,34 @@
 ﻿
 using ConsoleAWSSecrets.Helpers;
 using ConsoleAWSSecrets.Models;
+using Microsoft.Extensions.DependencyInjection;
 using Newtonsoft.Json;
 
 Console.WriteLine("----------------- SECRETS---------");
+Console.WriteLine("");
 string mySecret = await HelperSecretManager.GetSecretAsync();
 Console.WriteLine(mySecret);
 
 KeysModel model = JsonConvert.DeserializeObject<KeysModel>(mySecret);
-Console.WriteLine(model);
+//Console.WriteLine(model);
 Console.WriteLine(model.Mysql);
 
+
+// almacenamos el objeto keysmodel dentro de DY
+
+var proveedor =
+    new ServiceCollection()
+    .AddSingleton<Test>()
+    .AddSingleton<KeysModel>(x => model)
+    .BuildServiceProvider();
+
+Console.WriteLine("");
+// en cualquier clase/injection podemos recuperar las keys
+var service = proveedor.GetService<KeysModel>();
+string conn = service.Mysql;
+Console.WriteLine(conn);
+
+var test = proveedor.GetService<Test>();
+Console.WriteLine("API: " + test.GetValue());
+Console.WriteLine("");
 Console.WriteLine("----------------- FIN SECRETS---------");
